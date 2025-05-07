1c2eaf90336ba611e1c1cfeb250b0cb14d1f9665
afropapp13 <afropapp@yahoo.gr>
2025-05-07 10:49:33 -0500

    May 7 2025: processing correct atmospherics sample

--- loop_generators.cxx
+++ loop_generators.cxx
@@ -3,7 +3,7 @@
 
 	//----------------------------------------//
 	 	
-	WhichSample.push_back("/exp/uboone/app/users/apapadop/BuildEventGenerators/jobcards/atmo/14_1000180400_CC_v3_6_0_AR23_20i_00_000.flat.root"); WhichName.push_back("atmo_AR23_20i_00_000");	
+	WhichSample.push_back("/pnfs/uboone/persistent/users/apapadop/GENIETweakedSamples/v3_6_0_AR23_20i_00_000/dune_atmospherics/14_1000180400_CC_v3_6_0_AR23_20i_00_000.flat.root"); WhichName.push_back("atmo_AR23_20i_00_000");	
 	WhichSample.push_back("/pnfs/uboone/persistent/users/apapadop/GENIETweakedSamples/v3_4_2_AR23_20i_00_000_CC/14_1000180400_CC_v3_4_2_AR23_20i_00_000.flat.root"); WhichName.push_back("bnb_AR23_20i_00_000");			 
 
 	//----------------------------------------//
